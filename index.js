634faf7ee9be1090c3bb4b9fcc6e66ead248851d
sazypapi <128397579+sazypapi@users.noreply.github.com>
2024-03-17 15:15:51 +0100

    No Scroll

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const remove = document.querySelector(".remove-hamburger");
 const big = document.querySelector(".big");
+const body = document.querySelector("body");
 hamburger.addEventListener("click", () => {
   big.classList.add("active");
+  bodyScrollLockUpgrade.disableBodyScroll(body);
 });
 remove.addEventListener("click", () => {
   big.classList.remove("active");
+  bodyScrollLockUpgrade.enableBodyScroll(body);
 });
