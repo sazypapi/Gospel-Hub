c5dd69958138474483305ec35f9cff238e239a79
sazypapi <128397579+sazypapi@users.noreply.github.com>
2024-03-25 18:54:58 +0100

    Some mobile media query changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const remove = document.querySelector(".remove-hamburger");
 const big = document.querySelector(".big");
 const body = document.querySelector("body");
+const navbar = document.querySelector("nav");
 hamburger.addEventListener("click", () => {
   big.classList.add("active");
   bodyScrollLockUpgrade.disableBodyScroll(body);
@@ -10,3 +11,16 @@ remove.addEventListener("click", () => {
   big.classList.remove("active");
   bodyScrollLockUpgrade.enableBodyScroll(body);
 });
+
+let prevScrollPos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-100px"; // Adjust this value based on your navbar height
+  }
+
+  prevScrollPos = currentScrollPos;
+};
