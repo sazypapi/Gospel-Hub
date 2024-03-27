a450325b3e8aee19faa446bb3cee76031253efd0
sazypapi <128397579+sazypapi@users.noreply.github.com>
2024-03-27 11:48:51 +0100

    More changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const remove = document.querySelector(".remove-hamburger");
 const big = document.querySelector(".big");
 const body = document.querySelector("body");
-const navbar = document.querySelector("nav");
+// const navbar = document.querySelector("nav");
 hamburger.addEventListener("click", () => {
   big.classList.add("active");
   bodyScrollLockUpgrade.disableBodyScroll(body);
@@ -16,15 +16,30 @@ remove.addEventListener("click", () => {
   hamburger.style.display = "inline-block";
 });
 
-let prevScrollPos = window.pageYOffset;
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
+// let prevScrollPos = window.pageYOffset;
+// window.onscroll = function () {
+//   let currentScrollPos = window.pageYOffset;
 
-  if (prevScrollPos > currentScrollPos) {
-    navbar.style.top = "0";
+//   if (prevScrollPos > currentScrollPos) {
+//     navbar.style.top = "0";
+//   } else {
+//     navbar.style.top = "-10vh"; // Adjust this value based on your navbar height
+//   }
+
+//   prevScrollPos = currentScrollPos;
+// };
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop && currentScroll > 0) {
+    // Scrolling down, hide navbar
+    document.querySelector("navbar").style.transform = "translateY(-100%)";
   } else {
-    navbar.style.top = "-10vh"; // Adjust this value based on your navbar height
+    // Scrolling up, show navbar
+    document.querySelector("navbar").style.transform = "translateY(0)";
   }
 
-  prevScrollPos = currentScrollPos;
-};
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
